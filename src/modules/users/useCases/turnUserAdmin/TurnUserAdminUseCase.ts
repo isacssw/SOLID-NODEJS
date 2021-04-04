1358d756f684b3d8e3c939884d1833bf7215dd3a
Isac X <isacssw@gmail.com>
2021-04-04 19:23:34 +0100

    refactoring routes and cases

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
@@ -9,7 +9,15 @@ class TurnUserAdminUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new Error("User do not exist");
+    }
+
+    const newUserAdmin = this.usersRepository.turnAdmin(user);
+
+    return newUserAdmin;
   }
 }
 
